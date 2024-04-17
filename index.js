bc698ee307c1cfca12f7bb564a1eb60a60afde78
JoanMburu <joan.wangui@student.moringaschool.com>
2024-04-17 20:57:05 +0300

    find method lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(arrayOfObjects) {
+    const win = arrayOfObjects.find(object => object.result === "W");
+    return win ? win.year : undefined;
+}
+
+const games = [
+    { year: 2020, result: "L" },
+    { year: 2021, result: "W" },
+    { year: 2022, result: "L" },
+    { year: 2023, result: "W" }
+];
+
+console.log(superbowlWin(games));
